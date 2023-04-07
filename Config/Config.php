ba5d3ac39670374dec7c26b2481cbc3645fc1461
Javier <jamj1702@yahoo.es>
2023-04-07 09:22:36 -0600

    subir config

---
+++ Config/Config.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * 
+ * Poner URL por default
+ * Poner constantes de conexion
+ * Zona horaria
+ * Simbolo de moneda
+ * Datos variantes sistema
+ * 
+ */
+
+/*----------  https://jip.grupopaniagua.com/
+	Ruta o dominio del servidor  - Server path or domain
+----------*/
+
+#URL development
+//const BASE_URL = "http://localhost/jipsafety";
+
+#URL production
+const BASE_URL = "https://jip.grupopaniagua.com/";
+
+
+/*----------  
+	Nombre de la empresa o compañia -  Company or company name
+----------*/
+
+const COMPANY = "ma-sistema";
+
+/*----------  
+	Idioma - Language
+    Español -> es 
+----------*/
+
+const LANG = "es";
+
+/*----------  
+		Palabra clave dashboard - Dashboard keyword
+		No usar los siguientes valores - Do not use the following values
+
+		index | product | bag | registration | details | signin
+----------*/
+
+const DASHBOARD = "admin";
+
+/*----------  
+	Nombre de la sesion -  Session name
+----------*/
+
+const SESSION_NAME = "STO";
+
+
+/*----------  Redes sociales - Social networks  ----------*/
+const FACEBOOK = "https://www.facebook.com/CarlosAlfaroES/";
+const INSTAGRAM = "";
+const YOUTUBE = "https://www.youtube.com/c/CarlosAlfaro007";
+const TWITTER = "";
+
+
+/*----------  Configuración de moneda - Currency Settings  ----------*/
+const COIN_SYMBOL = "$";
+const COIN_NAME = "USD";
+const COIN_DECIMALS = "2";
+const COIN_SEPARATOR_THOUSAND = ",";
+const COIN_SEPARATOR_DECIMAL = ".";
+
+
+#CONEXION
+
+#Production
+const DB_HOST = "82.180.174.103";
+const DB_NAME = "u558030020_avenger";
+const DB_USER = "u558030020_avenger";
+const DB_PASSWORD = "/EY09k!Y5i;";
+const DB_CHARSET = "charset=utf8";
+
+
+#Development
+/*const DB_HOST = "localhost";
+const DB_NAME = "jipsafety";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "charset=utf8";*/
+
+ #Zona horaria
+ #date_default_timezone_set('America/Nicaragua');
+
+ #Simbolo de moneda
+
+ #Datos variantes del sistema
